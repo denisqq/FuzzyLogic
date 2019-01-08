2a70d21c2208dc29a6592e95fea37f5be9499f5b
Denis Tretiakov <frrait98@gmail.com>
2019-01-09 00:12:01 +0500

    test

--- src/main/test/LogicTest.java
+++ src/main/test/LogicTest.java
@@ -16,14 +16,12 @@ import java.util.List;
 import java.util.Map;
 
 public class LogicTest {
-
-
     @Test
     public void logicTest() {
         Logic logic = new Logic();
         List<Double> values = Arrays.asList(7.0D, 15.0D);
         Triangle onFoot = new Triangle(0.0D, 7.0D, 3.0D);
-        Triangle onCar = new Triangle(4.0D,15.0D,8.0D);
+        Triangle onCar = new Triangle(4.0D, 15.0D, 8.0D);
 
         LTrapezoid big = new LTrapezoid(4.0D, 8.0D);
         RTrapezoid small = new RTrapezoid(0.0D, 5.0D);
@@ -33,41 +31,41 @@ public class LogicTest {
 
         Rule r1 = new Rule();
         r1.setConditionList(Arrays.asList(
-                new Condition(big,"Большая", new Variable(0)),
-                new Condition(big,"Большая", new Variable(1))
+                new Condition(big, "Большая", new Variable(0)),
+                new Condition(big, "Большая", new Variable(1))
         ));
         r1.setConclusion(
-                new Conclusion(onCar,"На машине", new Variable(0),1.0D)
+                new Conclusion(onCar, "На машине", new Variable(0), 1.0D)
         );
         rules.add(r1);
 
         Rule r2 = new Rule();
         r2.setConditionList(Arrays.asList(
-                new Condition(small,"Маленькая", new Variable(0)),
-                new Condition(small,"Маленькая", new Variable(1))
+                new Condition(small, "Маленькая", new Variable(0)),
+                new Condition(small, "Маленькая", new Variable(1))
         ));
         r2.setConclusion(
-                new Conclusion(onFoot,"Пешком", new Variable(0),1.0D)
+                new Conclusion(onFoot, "Пешком", new Variable(0), 1.0D)
         );
         rules.add(r2);
 
         Rule r3 = new Rule();
         r3.setConditionList(Arrays.asList(
-                new Condition(big,"Большая", new Variable(0)),
-                new Condition(small,"Маленькая", new Variable(1))
+                new Condition(big, "Большая", new Variable(0)),
+                new Condition(small, "Маленькая", new Variable(1))
         ));
         r3.setConclusion(
-                new Conclusion(onFoot,"Бол->Мал", new Variable(0),0.5D)
+                new Conclusion(onFoot, "Бол->Мал", new Variable(0), 0.5D)
         );
         rules.add(r3);
 
         Rule r4 = new Rule();
         r4.setConditionList(Arrays.asList(
-                new Condition(small,"Маленькая", new Variable(0)),
-                new Condition(big,"Большая", new Variable(1))
+                new Condition(small, "Маленькая", new Variable(0)),
+                new Condition(big, "Большая", new Variable(1))
         ));
         r4.setConclusion(
-                new Conclusion(onCar,"Мал->Бол", new Variable(0),0.5D)
+                new Conclusion(onCar, "Мал->Бол", new Variable(0), 0.5D)
         );
         rules.add(r3);
 
